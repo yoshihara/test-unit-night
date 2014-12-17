37dc3d2d3a320838b1006373f7b46d58003ac9ec
yoshihara <h.yoshihara@everyleaf.com>
2014-12-18 00:10:54 +0900

    Implement lesson3

---
+++ lesson3/test/user-test.rb
@@ -0,0 +1,35 @@
+require "user"
+
+class UserTest < Test::Unit::TestCase
+  sub_test_case "#full_name" do
+    test "localeがjaのとき" do
+      user = User.new("ja")
+      user.last_name = "sato"
+      user.first_name = "hanako"
+
+      assert_equal("sato hanako", user.full_name)
+    end
+
+    test "localeがenの時" do
+      user = ::User.new("en")
+      user.last_name = "sato"
+      user.first_name = "hanako"
+
+      assert_equal("hanako sato", user.full_name)
+    end
+  end
+
+  sub_test_case "#ja?" do
+    test "何も指定しない時" do
+      user = User.new
+
+      assert { user.ja? }
+    end
+
+    test "'en'を指定した時" do
+      user = User.new("en")
+
+      assert { !user.ja? }
+    end
+  end
+end
